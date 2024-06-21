dc93f52b6e7a24efd3b74f3c53e57fb0f78ce21d
Arexxa <shahrulayol31@gmail.com>
2024-06-21 20:15:12 +0800

    resize fonts

--- src/pages/WomeGlobeSection/index.js
+++ src/pages/WomeGlobeSection/index.js
@@ -44,14 +44,14 @@ function WomeGlobeSection() {
         <div className='relative'>
           <div className='my-12 xs:px-4'>
             {!isMobile && (
-              <div className='helloworld-text sm:text-[40px] md:text-[6.125rem] lg:text-[6rem] xl:text-[8rem] 2xl:text-[12rem]'>
+              <div className='helloworld-text sm:text-[40px] md:text-[6.125rem] lg:text-[6rem] xl:text-[8rem] 2xl:text-[10rem]'>
               Hello World
             </div>
             )}
-            <div className='Montserrat font-medium xs:text-[20px] sm:text-[20px] md:text-[22px] lg:text-[24px] xl:text-[28px] 2xl:text-[40px] md:leading-[28px] lg:leading-[32px] xl:leading-[34.13px] text-[#AAAAAA]'>
+            <div className='Montserrat font-medium xs:text-[20px] sm:text-[20px] md:text-[22px] lg:text-[24px] xl:text-[28px] 2xl:text-[30px] md:leading-[28px] lg:leading-[32px] xl:leading-[34.13px] text-[#AAAAAA]'>
               Welcome to a memeverse experience for the masses.
             </div>
-            <div className='pt-4 Montserrat font-medium xs:text-[16px] sm:text-[14px] md:text-[16px] lg:text-[18px] xl:text-[20px] 2xl:text-[30px] md:leading-[20px] lg:leading-[22px] xl:leading-[24.38px] text-[#AAAAAA]'>
+            <div className='pt-4 Montserrat font-medium xs:text-[16px] sm:text-[14px] md:text-[16px] lg:text-[18px] xl:text-[20px] 2xl:text-[20px] md:leading-[20px] lg:leading-[22px] xl:leading-[24.38px] text-[#AAAAAA]'>
               Begin your journey into the World Of Meme with us.
             </div>
           </div>
