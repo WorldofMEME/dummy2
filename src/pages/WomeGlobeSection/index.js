a64b7b23d8a37654b73502828a7a6763c59fafa6
Arexxa <shahrulayol31@gmail.com>
2024-06-21 16:45:02 +0800

    adjust padding top for globe

--- src/pages/WomeGlobeSection/index.js
+++ src/pages/WomeGlobeSection/index.js
@@ -20,7 +20,7 @@ function WomeGlobeSection() {
   const isMobile = width <= 640;
 
   return (
-    <section className={`content1 xs:pt-[4rem] md:pt-[13rem] lg:pt-[13rem]  ${isMobile ? 'mobile-bg' : 'desktop-bg'}`}>
+    <section className={`content1 xs:pt-[4rem] md:pt-[18rem] lg:pt-[19rem] xl:pt-[19rem]  ${isMobile ? 'mobile-bg' : 'desktop-bg'}`}>
       <div className='animation-container'>
         <div className='absolute'>
           <img src={`${GlobeGif}?${new Date().getTime()}`} className='relative z-10' alt='globe' />
